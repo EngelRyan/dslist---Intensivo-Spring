139f708b51bd38ef060880432d9c87d1746f3ba3
engelryan <ryaneengel@gmail.com>
2025-01-23 02:13:05 -0300

    Feat: ListRepository,ListService,ListController

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,6 +4,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,8 +33,18 @@ public class GameService {
 
     @Transactional(readOnly = true)
     public ResponseEntity<?> findById(Long id) {
+
         return gameRepository.findById(id)
                 .map(game -> ResponseEntity.ok(new GameDTO(game)))
                 .orElseGet(() -> ResponseEntity.status(404).build());
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+
+        return result.stream().
+                map(GameMinDTO::new)
+                .toList();    }
 }
