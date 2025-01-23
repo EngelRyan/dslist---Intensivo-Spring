3786378fc0471b75ebbbd7ab5e1a6aa6e39d2b94
engelryan <ryaneengel@gmail.com>
2025-01-23 01:40:38 -0300

    Feat: Get FindById

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dslist.services;
 
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +18,7 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
 
         List<Game> listGames = gameRepository.findAll();
@@ -24,5 +28,12 @@ public class GameService {
         return listGames.stream().
                 map(GameMinDTO::new) //expresão lambda que transforma o objeto usando o construtor de GameMinDTO recebendo um Game
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<?> findById(Long id) {
+        return gameRepository.findById(id)
+                .map(game -> ResponseEntity.ok(new GameDTO(game)))
+                .orElseGet(() -> ResponseEntity.status(404).build());
     }
 }
